4cd0d07917a61d683acf106a1d1b4af070ea9ebb
Ida Benedetto <ida@uncommonplaces.com>
2013-03-28 19:31:41 -0400

    the inevitable CSS futzing, take 1

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <?php require_once("inc/connection.php"); ?>
 <?php require_once("inc/functions.php"); ?>
 <?php include ("inc/header.php"); ?>
+<div id="intro">
 <p>
-"You forget your victories, but you remember the losses."<br />
-This little application serves up personal affirmations and lost memories of victory. It's handy for when I've forgotten what I'm worth.
+"You forget your victories, but you remember the losses."</p>
+<p>This little application serves up personal affirmations and lost memories of victory. It's handy for when I've forgotten what I'm worth.
 </p>
+</div>
 		<section id="submit-aff">
 			
 
@@ -15,9 +17,9 @@ This little application serves up personal affirmations and lost memories of vic
 				<h3>Contribute an affirmation to Ida's Esteem Engine </h3>
 				<p>Help me defeat the monsters of self-doubt.  <br />
 					<br />Examples of things to contribute:<br />
-					- A personal anecdote about working together <br />
-					- A loving piece of advice <br />
-					- A description of something I might not know I'm awesome at<br />
+					<ul><li> A personal anecdote about working together </li> 
+					<li> A loving piece of advice </li> 
+					<li> A description of something I might not know I'm awesome at</li> </ul>
 					  (The more specific you can be, the better.)
 					<br /><textarea name="affirmation" rows="10" value="" id="affirmation" /></textarea>
 				</p>
@@ -47,7 +49,8 @@ This little application serves up personal affirmations and lost memories of vic
 					</select>
 
 				<p> Your Name (optional) <br /><input type="text" name="sub_name" value="" id="sub_name" /></p>
-				<p> Your email address for a personal thank you note (optional) <br /><input type="email" name="email" value="" id="email" /> <br /></p>
+				<p> Your email address (optional) <br /><input type="email" name="email" value="" id="email" /> <br />
+				 <span class="sm">I'll send you a personal thank you note when the engine churns up your affirmation.</span> </p>
 				<p><input type="submit" value="Submit Affirmation" /></p>
 				<?php if(isset($_GET['aff'])) {
 					if ($_GET['aff'] == 1) {
